c156ddf0aecaa58e0b2b4c54d85094008ffc7da1
it23392830 <it23392830@my.sliit.lk>
2026-01-30 21:55:19 +0530

    add metadata and suppress hydration warning in RootLayout component

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -1,16 +1,23 @@
 import "./globals.css";
 import Providers from "./providers";
 
+export const metadata = {
+  title: "AlgoBrave Assignment",
+  description: "Full Stack User Management App",
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <Providers>{children}</Providers>
       </body>
     </html>
   );
 }
+
+
